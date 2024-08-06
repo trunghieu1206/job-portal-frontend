0a81e554917cc9c3ca60bbb81aa61824bd01d643
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-08-06 17:45:51 +0700

    completed home page with minimal functionalities

---
+++ src/scripts/jobs.js
@@ -0,0 +1,24 @@
+export  const jobs = [
+    {
+        id: "1",
+        title: "Software Engineering",
+        place: "Hanoi",
+        company: "Company 1",
+        salary: "10000 - 12000",
+    },
+    {
+        id: "2",
+        title: "Backend Developer",
+        place: "Ho Chi Minh City",
+        company: "Company 2",
+        salary: "20000 - 30000",
+    },
+    {
+        id: "3",
+        title: "Cloud Engineer",
+        place: "Danang",
+        company: "Company 3",
+        salary: "50000 - 60000",
+    },
+
+]
---
+++ src/scripts/main.js
@@ -0,0 +1,45 @@
+import { jobs } from "./jobs.js";
+
+function renderFeaturedJobs() {
+    let featuredJobsHTML = `
+    <p>Featured Jobs</p>
+    <hr>
+    `;
+
+    jobs.forEach((job) => {
+        featuredJobsHTML += `
+        
+        <div class="job js-job">
+            <div class="logo">
+                <img class="background" src="images/job_logo.jpg" alt="">
+                <img class="top" src="images/company_logo.png" alt="">
+            </div>
+
+            <div class="contents">
+                <div class="job-title">
+                    ${job.title}
+                </div>
+                
+                <div class="job-description">
+                    <div class="job-place">
+                        <i style='font-size:20px' class='fas'>&#xf3c5;</i> ${job.place}
+                    </div>
+                    <div class="job-company">
+                        <i style='font-size:20px' class='fas'>&#xf1ad;</i> ${job.company}
+                    </div>
+                    <div class="job-salary">
+                        <i style='font-size:20px' class='fas'>&#xf155;</i> <span style="background-color: rgb(253, 106, 106); color: white; padding: 5px; border-radius: 10px;">${job.salary} USD</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        `;
+    });
+
+    console.log(featuredJobsHTML);
+
+    document.querySelector('.js-featured-jobs').innerHTML = featuredJobsHTML;
+}
+
+renderFeaturedJobs();
